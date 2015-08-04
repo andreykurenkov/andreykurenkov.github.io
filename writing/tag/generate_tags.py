35a04468179f84aa3b1dc25ff2a3476a609a7844
andreykurenkov <andrey.sendme@gmail.com>
2015-08-04 02:43:00 -0700

    Work on tags, some cleanup, revised image

--- writing/tag/generate_tags.py
+++ writing/tag/generate_tags.py
@@ -1,4 +1,5 @@
 tags = ['art','criticism','quasi-essay','thoughts','philosophy','food','videogames','film','culture','meta','life']
+text = {'quasi-essay':'Quasi Essay'}
 for tag in tags:
     with open('%s.md'%tag,'w') as tag_page:
         tag_page.write('---\n')
@@ -8,3 +9,9 @@ for tag in tags:
         tag_page.write('---\n')
         tag_page.write('{% include _posts_by_tag.html %}')
 
+
+with open('../../_data/tags.yml','w') as tag_data:
+  for tag in tags:
+      tag_data.write('%s:\n'%tag)
+      tag_data.write('  name: %s\n'%(text[tag] if tag in text else tag.title()))
+      tag_data.write('\n')
