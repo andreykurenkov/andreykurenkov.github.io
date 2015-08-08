4312843e4fe8bb9a2089b97376f8832e3a2e3b7c
andreykurenkov <andrey.sendme@gmail.com>
2015-08-08 02:49:32 -0700

    Improved look for mobile

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,23 +11,6 @@ var changeClass = function (r,className1,className2) {
     }
     return r.className;
 };  
-//  Creating our button in JS for smaller screens
-var menuElements = document.getElementById('site-nav');
-menuElements.insertAdjacentHTML('afterBegin','<button type="button" role="button" id="menutoggle" class="navtoogle navicon-lines-button x" aria-hidden="true"><span class="navicon-lines"></span>menu</button>');
-
-//  Toggle the class on click to show / hide the menu
-document.getElementById('menutoggle').onclick = function() {
-  changeClass(this, 'navtoogle active', 'navtoogle');
-};
-// http://tympanus.net/codrops/2013/05/08/responsive-retina-ready-menu/comment-page-2/#comment-438918
-document.onclick = function(e) {
-  var mobileButton = document.getElementById('menutoggle'),
-    buttonStyle =  mobileButton.currentStyle ? mobileButton.currentStyle.display : getComputedStyle(mobileButton, null).display;
-
-  if(buttonStyle === 'block' && e.target !== mobileButton && new RegExp(' ' + 'active' + ' ').test(' ' + mobileButton.className + ' ')) {
-    changeClass(mobileButton, 'navtoogle active', 'navtoogle');
-  }
-};
 
 /*! Plugin options and other jQuery stuff */
 
