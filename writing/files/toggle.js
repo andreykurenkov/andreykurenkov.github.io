31e4405cf77b4edb24d6d011d46d9bcdf44b75c2
andreykurenkov <andreyvkurenkov@gmail.com>
2020-04-03 03:34:32 -0700

    Site updated at: 2020-04-03 10:34:31 UTC

--- writing/files/toggle.js
+++ writing/files/toggle.js
@@ -1,26 +1,10 @@
-var displayingCategorical = true;
 $(function() {
-    var form = document.querySelector("#writingToggleForm");
-    form.addEventListener("change", function(event) {
+    var currentDisplay = $("#index > div:visible");
+    document.querySelector("#writingToggleForm").addEventListener("change", function(event) {
         console.log("Toggling writing display!");
-        if(displayingCategorical){
-          $('#categorical').fadeToggle(function() {
-              $('#chronological').fadeToggle(function() {
-              });
-          });
-        }else{
-          $('#chronological').fadeToggle(function() {
-              $('#categorical').fadeToggle(function() {
-              });
-          });
-        }
-        displayingCategorical = !displayingCategorical;
+        currentDisplay.fadeToggle(function() {
+            currentDisplay = $(event.srcElement.getAttribute("data-st-contentid"));
+            currentDisplay.fadeToggle();
+        });
     });
-    $("#chronological").fadeToggle(function() {
-         $("#chronological").css("visibility", "visible");
-         $("#chronological").css("position", "static");
-    });
-    /*Reset selection*/
-    form.reset();
-});
-
+})
