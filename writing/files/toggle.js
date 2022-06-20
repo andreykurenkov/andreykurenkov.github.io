15a0adb78071e0e9835ea878d46d83ad2ea48a88
andreykurenkov <andreyvkurenkov@gmail.com>
2022-06-20 01:19:35 -0700

    Site updated at: 2022-06-20 08:19:34 UTC

--- writing/files/toggle.js
+++ writing/files/toggle.js
@@ -1,10 +1,13 @@
+function change(event){
+   var currentDisplay = $("#index > div:visible");
+      console.log("Toggling writing display!");
+      currentDisplay.fadeToggle(function() {
+          currentDisplay = $(event.srcElement.getAttribute("data-st-contentid"));
+          currentDisplay.fadeToggle();
+      });
+};
 $(function() {
     var currentDisplay = $("#index > div:visible");
-    document.querySelector("#writingToggleForm").addEventListener("change", function(event) {
-        console.log("Toggling writing display!");
-        currentDisplay.fadeToggle(function() {
-            currentDisplay = $(event.srcElement.getAttribute("data-st-contentid"));
-            currentDisplay.fadeToggle();
-        });
-    });
+    console.log(document.querySelector(".form"));
+    document.querySelector(".form").addEventListener("change",change);
 })
